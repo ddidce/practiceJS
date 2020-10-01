a320a006eaa6862aa4c34943c92ebe977f0bf946
unknown <ddidce@gmail.com>
2020-10-01 22:08:15 +0900

    ES6적극적으로 사용, 미리 계산해서 변수로 사용.

--- 1.js
+++ 1.js
@@ -2,10 +2,10 @@ const rankingList = [
     'a','b','c','d','e','섹시승학','자스고수승학','승학센세'
 ]
 
-for(var i = 0 ; i < rankingList.length ; i++) {
-    var ranking = rankingList[i];
+for(let i = 0, rankingListLength =  rankingList.length ; i < rankingListLength ; i++) {
+    let ranking = rankingList[i];
     console.log(ranking);
-    var li = document.createElement('li');
+    let li = document.createElement('li');
     li.innerHTML = ranking;
     document.getElementById('list').appendChild(li);
 }
