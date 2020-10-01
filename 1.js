b3f95a403f7310754b50992526c271dfc5d578d0
Youtube <46154929+ddidce@users.noreply.github.com>
2020-10-01 23:24:09 +0900

    Ranking practice (#2)
    * Ranking practice

---
+++ 1.js
@@ -0,0 +1,10 @@
+const rankingList = [
+    'a','b','c','d','e','섹시승학','자스고수승학','승학센세'
+];
+
+for(let i = 0, rankingListLength = rankingList.length; i < rankingListLength; i++) {
+    let ranking = rankingList[i];
+    let li = document.createElement('li');
+    li.innerHTML = ranking;
+    document.getElementById('list').appendChild(li);
+};
